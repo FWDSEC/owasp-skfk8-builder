0f6f5a30519e98f9070005f4482bf58a5899a87b
Kurt Hundeck <k.hundeck@fwdsec.com>
2022-10-07 18:00:03 -0400

    -Fixing template issue from SECTOR presentation.

--- miniskf.ubuntu.sh
+++ miniskf.ubuntu.sh
@@ -6,8 +6,8 @@
 # | | | | | | | | | | |   <| |_| | |_) |  __/
 # |_| |_| |_|_|_| |_|_|_|\_\\__,_|_.__/ \___|
 #                                                                                      
-export K8_VERSION="v1.23.1"
-export COMPOSE_TPL="skfk8/minikube/docker-compose.tpl.yml"
+export K8_VERSION="v1.21.14"
+export TEMPLATE="skfk8/minikube/docker-compose.tpl.yml"
 export COMPOSE_FILE="skfk8/minikube/docker-compose.yml"
 
 export PERL='perl -i -pe' ##//perl exists on default OSX and Ubuntu. :)
@@ -16,20 +16,22 @@ export PERL='perl -i -pe' ##//perl exists on default OSX and Ubuntu. :)
     ##According to docs v1.23 is not supposed to support 'api_version="networking.k8s.io/v1beta1"' but seems to be with minikube (?)
     minikube start --kubernetes-version=$K8_VERSION --embed-certs=true --force-systemd=true
     
-    cp $COMPOSE_TPL $COMPOSE_FILE
+    cp $TEMPLATE $COMPOSE_FILE
 
-    ##Reads the $COMPOSE_TPL and replaces with values to use minikube
+    ##Reads the $TEMPLATE and replace$COMPOSE_FILEs with values to use minikube as the lab cluster
     $PERL "s#REPLACE_ME:/home/user_api/.kube/config#${HOME}/.kube/config:/home/user_api/.kube/config#g" $COMPOSE_FILE
     $PERL "s/LABS_KUBE_CONF=REPLACE_ME/LABS_KUBE_CONF=`cat ${HOME}/.kube/config | base64 | tr -d '\n'`/g" $COMPOSE_FILE
     $PERL "s#FRONTEND_URI=REPLACE_ME#FRONTEND_URI=http://localhost#g" $COMPOSE_FILE
     $PERL "s#SKF_API_URL=REPLACE_ME#SKF_API_URL=http://127.0.0.1/api#g" $COMPOSE_FILE
     $PERL "s#SKF_LABS_DOMAIN=REPLACE_ME#SKF_LABS_DOMAIN=http://$(minikube ip)#g" $COMPOSE_FILE
-    $PERL "s#:4.X.X#:4.1.0#g" $COMPOSE_FILE
+    $PERL "s#REPLACE_ME:/etc/nginx/nginx.conf#./site.conf:/etc/nginx/nginx.conf#g" $COMPOSE_FILE
+    $PERL "s#:X.Y.Z#:4.1.0#g" $COMPOSE_FILE
     
+    ##Use Docker to host the front-end/API etc. which is configured to use the minikube labs cluster
     docker-compose -f $COMPOSE_FILE up -d
 }
 
-011-minikube-iptables() {
+020-minikube-iptables() {
     ##Connect the bridges between minikube and docker
     export brmini=`brctl show|egrep -ie '^br.+veth.*'|cut -f 1|head -n1`
     export brdocker=`brctl show|egrep -ie '^br.+veth.*'|cut -f 1|tail -n1`
@@ -39,7 +41,7 @@ export PERL='perl -i -pe' ##//perl exists on default OSX and Ubuntu. :)
     sudo iptables -I DOCKER-USER -i $brdocker -o $brmini -j ACCEPT
 }
 
-020-destroy-minikube() {
+999-destroy-minikube() {
     ###DESTROY ALL TRACES and invalidate the configuration above.
     docker-compose -f $COMPOSE_FILE down --volumes
     minikube delete --all
@@ -47,6 +49,4 @@ export PERL='perl -i -pe' ##//perl exists on default OSX and Ubuntu. :)
     ##Clean-up iptables mapping from non-exitent br (de-composed, and minikube --delete)
     sudo iptables -D DOCKER-USER -i $brmini -o $brdocker -j ACCEPT
     sudo iptables -D DOCKER-USER -i $brdocker -o $brmini -j ACCEPT
-
-    #sudo rm -fr ~/.kube/ ~/.minikube/
 }
