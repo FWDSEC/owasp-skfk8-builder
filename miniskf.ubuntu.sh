fb8744ec5edff3a9b6040b0da0b04811a4b2ea97
Kurt Hundeck <k.hundeck@fwdsec.com>
2022-10-07 13:48:39 -0400

    -Fixing template issue from SECTOR presentation.

--- miniskf.ubuntu.sh
+++ miniskf.ubuntu.sh
@@ -10,21 +10,23 @@ export K8_VERSION="v1.23.1"
 export COMPOSE_TPL="skfk8/minikube/docker-compose.tpl.yml"
 export COMPOSE_FILE="skfk8/minikube/docker-compose.yml"
 
-export SED='sed -e' ##//Ubuntu setting for 'sed'
+export PERL='perl -i -pe' ##//perl exists on default OSX and Ubuntu. :)
 
 010-start-minikube() {
     ##According to docs v1.23 is not supposed to support 'api_version="networking.k8s.io/v1beta1"' but seems to be with minikube (?)
-    #minikube start --kubernetes-version=$K8_VERSION --embed-certs=true --force-systemd=true
+    minikube start --kubernetes-version=$K8_VERSION --embed-certs=true --force-systemd=true
+    
+    cp $COMPOSE_TPL $COMPOSE_FILE
 
     ##Reads the $COMPOSE_TPL and replaces with values to use minikube
-    $SED "s#REPLACE_ME:/home/user_api/.kube/config#${HOME}/.kube/config:/home/user_api/.kube/config#" $COMPOSE_TPL > $COMPOSE_FILE
-    $SED "s/REPLACE_ME/`cat ${HOME}/.kube/config | base64 | tr -d '\n'`/g" $COMPOSE_TPL > $COMPOSE_FILE
-    $SED "s#FRONTEND_URI=REPLACE_ME#FRONTEND_URI=http://localhost#" $COMPOSE_TPL > $COMPOSE_FILE
-    $SED "s#SKF_API_URL=REPLACE_ME#SKF_API_URL=http://127.0.0.1/api#" $COMPOSE_TPL > $COMPOSE_FILE
-    $SED "s#SKF_LABS_DOMAIN=REPLACE_ME#SKF_LABS_DOMAIN=http://$(minikube ip)#" $COMPOSE_TPL > $COMPOSE_FILE
-    $SED "s#:4.X.X#:4.1.0#" $COMPOSE_TPL > $COMPOSE_FILE
-
-    #docker-compose -f $COMPOSE_FILE up -d
+    $PERL "s#REPLACE_ME:/home/user_api/.kube/config#${HOME}/.kube/config:/home/user_api/.kube/config#g" $COMPOSE_FILE
+    $PERL "s/LABS_KUBE_CONF=REPLACE_ME/LABS_KUBE_CONF=`cat ${HOME}/.kube/config | base64 | tr -d '\n'`/g" $COMPOSE_FILE
+    $PERL "s#FRONTEND_URI=REPLACE_ME#FRONTEND_URI=http://localhost#g" $COMPOSE_FILE
+    $PERL "s#SKF_API_URL=REPLACE_ME#SKF_API_URL=http://127.0.0.1/api#g" $COMPOSE_FILE
+    $PERL "s#SKF_LABS_DOMAIN=REPLACE_ME#SKF_LABS_DOMAIN=http://$(minikube ip)#g" $COMPOSE_FILE
+    $PERL "s#:4.X.X#:4.1.0#g" $COMPOSE_FILE
+    
+    docker-compose -f $COMPOSE_FILE up -d
 }
 
 011-minikube-iptables() {
@@ -39,7 +41,7 @@ export SED='sed -e' ##//Ubuntu setting for 'sed'
 
 020-destroy-minikube() {
     ###DESTROY ALL TRACES and invalidate the configuration above.
-    docker-compose -f $COMPOSE_TPL down --volumes
+    docker-compose -f $COMPOSE_FILE down --volumes
     minikube delete --all
 
     ##Clean-up iptables mapping from non-exitent br (de-composed, and minikube --delete)
